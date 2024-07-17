ac73b4a8d6b7d98fc3e0049d0ed430b987a86a12
sivachandran06 <sivachandran997@gamil.com>
2024-07-17 16:23:27 +0530

    Signin Finished

--- src/Components/HeaderComponent/Signin.js
+++ src/Components/HeaderComponent/Signin.js
@@ -2,12 +2,11 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import PersonIcon from '@mui/icons-material/Person';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import Typography from '@mui/material/Typography';
-import { DialogActions, TextField } from '@mui/material';
-import AppleIcon from "@mui/icons-material/Apple";
+import PeopleAltSharpIcon from '@mui/icons-material/PeopleAltSharp';
+import AppleIcon from '@mui/icons-material'
+import './Login.css';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { White } from '@mui/material/colors';
 
 
 
@@ -15,15 +14,13 @@ import AppleIcon from "@mui/icons-material/Apple";
 
 export default function Signin() {
 
-  const [open, setOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
 
-  const handleClose = () => {
-    setOpen(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
   };
+ 
 
   return (
     <div > 
@@ -31,55 +28,51 @@ export default function Signin() {
       <Button variant="outlined"
         startIcon={<PersonIcon />}
         style={{ textTransform: "lowercase", color: "White", backgroundColor: "red", borderRadius: "5px", height: "25px", fontSize: "11.5px"}}
-        onClick={handleClickOpen}>
+        onClick={togglePopup}>
         Sign in
       </Button>
     </Stack>
-    <div>
-    <Dialog open={open} onclose={handleClickOpen}>
-        <Button onClick={handleClose}sx={{color:"#aeaead", display:"flex",placeself: "flex-start", backgroundColor:"#333336",borderradius: "50%",padding: "19px",fontWeight:"bold" }}> X </Button>
-
-        <div style={{ display: "flex", alignItems:"center", flexDirection:'column'}}>
-          <AppleIcon sx={{fontSize:'60px' ,color:"#6e6e73"}}/>
-          <DialogTitle >Sign In or Sign Up</DialogTitle>
-          <DialogContent>
-            <Typography variant="body1" gutterBottom>
-              Enter your email to get started.
-            </Typography>
-          </DialogContent>
+    {isOpen && (
+        <div className="popup">
+          <div className="popup-inner">
+            <button className="close-btn" onClick={togglePopup} style={{background: "none", background:"#1c1c1e",border: "13px"}}>
+             <CancelIcon sx={{ color: "action" ,fontSize:"40px"}}/>
+            </button>
+            <div className="popup-content">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
+                alt="Apple Logo"
+                className="logo"
+              />
+              <h1>Sign In or Sign Up</h1>
+              <p>Enter your email to get started.</p>
+              <input
+                type="email"
+                placeholder="Email or Apple Account"
+                required
+              />
+              <input
+                type="Password"
+                placeholder="Password"
+                required
+              />
+              <PeopleAltSharpIcon style={{color:"#fa2d48"}}/>
+              <p className="info-text">
+                Your Apple Account information is used to allow you to sign in
+                securely and access your data. Apple records certain data for
+                security, support, and reporting purposes. If you agree, Apple
+                may also use your Apple Account information to send you
+                marketing emails and communications, including based on your use
+                of Apple services.{' '}
+                <a href="#">See how your data is managed...</a>
+              </p>
+              <button className="continue-btn">Continue</button>
+            </div>
+          </div>
         </div>
-        <div style={{gap:"20px"}}>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="email"
-              label="Email or Apple Account"
-              type="email"
-              fullWidth
-              variant="outlined"
-
-            />
-            <Typography variant="body2" gutterBottom>
-              Your Apple Account information is used to allow you to sign in securely and access your data.
-              Apple records certain data for security, support, and reporting purposes. If you agree, Apple
-              may also use your Apple Account information to send you marketing emails and communications,
-              including based on your use of Apple services.
-            </Typography>
-            <Button variant="text" >
-              See how your data is managed.
-            </Button>
-          </DialogContent>
-        </div>
-
-        <DialogActions>
-          <Button onClick={handleClose} >
-            Continue
-          </Button>
-        </DialogActions>
-      </Dialog >
-    </div>
+      )}
     </div>
       
   );
 }
+
